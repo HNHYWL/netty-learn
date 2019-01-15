bbec81374f774ad1984c16eeed80505de9eeb7ed
Your Name <you@example.com>
2019-01-15 15:37:52 +0800

    init

---
+++ src/main/java/one/TimeClient.java
@@ -0,0 +1,36 @@
+package one;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeClient {
+    public void connect(int port, String host) throws InterruptedException {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap b = new Bootstrap();
+        b.group(group).channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new TimeClientHandler());
+                    }
+                });
+
+        ChannelFuture f = b.connect(host, port).sync();
+        f.channel().closeFuture().sync();
+
+        group.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new TimeClient().connect(9901, "localhost");
+    }
+}
---
+++ src/main/java/one/TimeServer.java
@@ -0,0 +1,40 @@
+package one;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeServer {
+    public void bind(int port) throws InterruptedException {
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast(new TimeServerHandler());
+                    }
+                });
+
+        ChannelFuture f = b.bind(port).sync();
+        f.channel().closeFuture().sync();
+
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        new TimeServer().bind(9901);
+    }
+}
